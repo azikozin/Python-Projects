1e28ee6c85956c65950eb208a9c66a996da94238
Azaan Shahid <Rizwan@elev.rogfk.no>
2025-01-28 09:05:49 +0100

    Added suport for more sites
    🔥 waowo

--- User Enumeration/sites.py
+++ User Enumeration/sites.py
@@ -12,5 +12,57 @@ SITES = [
         "url": "https://35photo.pro/@{username}",
         "check_redirect": True,
         "redirect_url": "https://35photo.pro/"
+    },
+    {
+        "name" : "7dach",
+        "url" : "https://7dach.ru/profile/{username}"
+    },
+    {
+        "name" : "Alik",
+        "url" : "https://www.alik.cz/u/{username}"
+    },
+    {
+        "name" : "Anonup",
+        "url" : "https://anonup.com/@{username}"
+    },
+    {
+        "name" : "Artbreeder",
+        "url" : "https://www.artbreeder.com/{username}"
+    },
+    {
+        "name" : "Artistsnclients",
+        "url" : "https://artistsnclients.com/people/{username}"
+    },
+    {
+        "name" : "Audiojungle",
+        "url" : "https://audiojungle.net/user/{username}"
+    },
+    {
+        "name" : "Bio.site",
+        "url" : "https://bio.site/{username}"
+    },
+    {
+        "name" : "Boosty",
+        "url" : "https://boosty.to/{username}"
+    },
+    {
+        "name" : "Coda",
+        "url" : "https://coda.io/@{username}"
+    },
+    {
+        "name" : "Dating",
+        "url" : "https://dating.ru/{username}"
+    },
+    {
+        "name" : "Dev",
+        "url" : "https://dev.to/{username}"
+    },
+    {
+        "name" : "Flickr",
+        "url" : "https://www.flickr.com/photos/{username}"
+    },
+    {
+        "name" : "Github",
+        "url" : "https://github.com/{username}"
     }
 ]
